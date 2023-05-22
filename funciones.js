78a3e477023f036a5e6613392cdc4ae5ae63e54c
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-05-21 21:31:26 -0300

    se agrega el cuerpo de la serpiente

--- funciones.js
+++ funciones.js
@@ -13,6 +13,8 @@ let snakeY = blockSize * 5;
 let velocidadX = 0;
 let velocidadY = 0;
 
+let snakeBody = [];
+
 // Dibujar comida
 let foodX;
 let foodY;
@@ -26,33 +28,51 @@ window.onload = function () {
     placeFood();
     document.addEventListener('keyup', changeDirection);
     // update();
-    setInterval(update, 1000/10);
+    setInterval(update, 1000 / 10);
 }
 
 function update() {
     context.fillStyle = 'black';
     context.fillRect(0, 0, board.width, board.height);
 
+    context.fillStyle = 'red';
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+
+    if (snakeX == foodX && snakeY == snakeY) {
+        snakeBody.push([foodX, foodY]);
+        placeFood();
+    }
+
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+        snakeBody[i] = snakeBody[i-1];
+    }
+    if(snakeBody.length){
+        snakeBody[0] = [snakeX, snakeY]
+    }
+
     context.fillStyle = 'yellow';
     snakeX += velocidadX * blockSize;
     snakeY += velocidadY * blockSize;
     context.fillRect(snakeX, snakeY, blockSize, blockSize);
 
-    context.fillStyle = 'red';
-    context.fillRect(foodX, foodY, blockSize, blockSize);
+    for (let i = 0; i < snakeBody.length; i++) {
+        context.fillRect(snakeBody[i][0], snakeBody[i][1], blockSize, blockSize);
+    }
+
+
 }
 
 function changeDirection(e) {
-    if (e.code == 'ArrowUp') {
+    if (e.code == 'ArrowUp' && velocidadY != 1) {
         velocidadX = 0;
         velocidadY = -1;
-    } else if (e.code == 'ArrowDown') {
+    } else if (e.code == 'ArrowDown' && velocidadY != -1) {
         velocidadX = 0;
         velocidadY = 1;
-    } else if (e.code == 'ArrowLeft') {
+    } else if (e.code == 'ArrowLeft' && velocidadX != 1) {
         velocidadX = -1;
         velocidadY = 0;
-    } else if (e.code == 'ArrowRight') {
+    } else if (e.code == 'ArrowRight' && velocidadX != -1) {
         velocidadX = 1;
         velocidadY = 0;
     }
