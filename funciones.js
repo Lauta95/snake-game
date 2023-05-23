c27143484cc2991f6c5ca3dc6cfecf4400cb5174
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-05-22 21:40:51 -0300

    creacion del recuadro donde se va a jugar

---
+++ funciones.js
@@ -0,0 +1,20 @@
+let blockSize = 25;
+let rows = 20;
+let cols = 20;
+let board;
+let context;
+
+window.onload = function () {
+    board = document.getElementById('board');
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext('2d');
+
+    update();
+}
+
+function update() {
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, board.width, board.height);
+
+}
