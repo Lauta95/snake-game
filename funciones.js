1d077dc6469b01b6078a13f78779054afa6426dd
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-05-22 22:06:48 -0300

    posicionamiento al azar de la comida

--- funciones.js
+++ funciones.js
@@ -9,12 +9,19 @@ let context;
 let snakeX = blockSize * 5;
 let snakeY = blockSize * 5;
 
+// Variables de la comida
+
+let foodX = blockSize * 10;
+let foodY = blockSize * 10;
+
 window.onload = function () {
     board = document.getElementById('board');
+    // Esto establece el alto y ancho del board, se multiplica porque cada bloquesito es de 25pixeles  y cada row y cada col es de 20pixeles.
     board.height = rows * blockSize;
     board.width = cols * blockSize;
     context = board.getContext('2d');
 
+    placeFood();
     update();
 }
 
@@ -23,5 +30,15 @@ function update() {
     context.fillRect(0, 0, board.width, board.height);
 
     context.fillStyle = 'yellow';
-    context.fillRect(snakeX, snakeY, blockSize, blockSize)
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    context.fillStyle = 'red';
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+// Acá se crea la funcion para posicionar la comida en un lugar al azar del mapa.
+
+function placeFood() {
+    foodX = Math.floor(Math.random() * cols * blockSize);
+    foodY = Math.floor(Math.random() * rows * blockSize);
 }
